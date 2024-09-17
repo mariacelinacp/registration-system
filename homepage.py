f206adf256967872d1c936eeb18d6be4ef90b52b
Maria Celina <mariacelinacpereira@gmail.com>
2024-09-17 10:16:47 -0300

    página de pesquisa adicionada

--- homepage.py
+++ homepage.py
@@ -1,37 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, request, redirect, url_for, flash
+from database import db
+from models import Cadastro
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cadastros.db'  # Caminho do banco de dados SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:mypassword@localhost:5432/registration_system'  # Caminho do banco de dados SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '3SA41F56WE4W561VE6AS4T3464R6WQ1SDF12'    
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/cadastros")
+@app.route("/cadastros", methods=['GET', 'POST'])
 def cadastros():
+    if request.method == 'POST':
+        search_query = request.form.get('search_query')
+        if search_query:
+            # Busca no banco de dados usando o input
+            resultados = Cadastro.query.filter(Cadastro.pet_name.ilike(f'%{search_query}%')).all()
+            if not resultados:
+                flash('Nenhum resultado encontrado.', category='info')
+            return render_template("resultados.html", resultados=resultados, search_query=search_query)
     return render_template("cadastros.html")
 
-@app.route("/novocadastro")
+
+@app.route("/novocadastro", methods=['GET', 'POST'])
 def novocadastro():
+    if request.method == 'POST':
+        info_pet_name = request.form.get('pet_name')
+        info_age = request.form.get('age')
+        info_weight = request.form.get('weight')
+        info_owner_name = request.form.get('owner_name')
+        info_phone = request.form.get('phone')
+        info_email = request.form.get('email')
+        info_current_date = request.form.get('current_date')
+
+        if not info_pet_name or not info_age or not info_weight or not info_owner_name or not info_phone or not info_current_date:
+            flash('Por favor, preencha todos os campos obrigatórios.', category='error')
+        else:
+            new_info = Cadastro(pet_name=info_pet_name, age=info_age, weight=info_weight, owner_name=info_owner_name, phone=info_phone, email=info_email, current_date=info_current_date)
+            db.session.add(new_info)
+            db.session.commit()
+            flash('Configuração salva!', category='success')
+
     return render_template("novocadastro.html")
-
-class Cadastro(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    pet_name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.String(50), nullable=False)
-    weight = db.Column(db.String(50), nullable=False)
-    owner_name = db.Column(db.String(100), nullable=False)
-    phone1 = db.Column(db.String(50), nullable=False)
-    phone2 = db.Column(db.String(50), nullable=True)
-    current_date = db.Column(db.String(50), nullable=False)
-
-    def __repr__(self):
-        return f'<Cadastro {self.pet_name}>'
 
 
 if __name__ == "__main__":
